71506cfa98498776b5d869d6e6dc221171f8ae58
RaulSaraujo <raul.araujo.2000@hotmail.com>
2023-04-19 01:27:03 -0300

    Correção colection

--- src/components/MoviesCollection.vue
+++ src/components/MoviesCollection.vue
@@ -1,14 +1,14 @@
 <template>
   <v-dialog
     v-model="dialog"
-    scrollable
     :height="!useDisplay.xs ? '80vh' : ''"
     :width="!useDisplay.xs ? '100vw' : ''"
     scrim="black"
     :fullscreen="useDisplay.xs"
+    scrollable
   >
-    <v-card :rounded="!useDisplay.xs ? 'xl' : ''" color="black">
-      <v-card-title style="background: rgb(0, 0, 0, 0.9)" class="text-h5 py-4">
+    <v-card v-if="!useDisplay.xs" :rounded="!useDisplay.xs ? 'xl' : ''">
+      <v-card-title class="text-h5 py-4">
         <v-row no-gutters>
           <span :class="!useDisplay.xs ? 'text-h4' : ''">
             {{ collectionMovies.name }}</span
@@ -22,91 +22,66 @@
           ></v-icon>
         </v-row>
       </v-card-title>
+      <v-divider></v-divider>
 
-      <v-card
-        v-for="parts in collectionMovies.parts"
-        :key="parts"
-        elevation="0"
-        color="rgba(0,0,0,0.1)"
-        rounded="xl"
-        :height="!useDisplay.xs ? '450' : '300'"
-        class="my-3 mx-5"
-      >
-        <v-row v-if="!useDisplay.xs" no-gutters>
-          <v-col cols="12" sm="12" md="3" lg="3" xl="2">
-            <v-img
-              :src="`https://image.tmdb.org/t/p/original${parts.poster_path}`"
-              :lazy-src="`https://image.tmdb.org/t/p/w300${parts.poster_path}`"
-              gradient="to right, rgba(0,0,0,0.6),rgba(0,0,0,0.4),rgba(0,0,0,0.3),rgba(0,0,0,0)"
-              cover
-              height="100%"
-            >
-              <template v-slot:placeholder>
-                <div class="d-flex align-center justify-center fill-height">
-                  <v-progress-circular
-                    color="grey-lighten-4"
-                    indeterminate
-                  ></v-progress-circular>
-                </div>
-              </template>
-            </v-img>
-          </v-col>
-          <v-col cols="12" sm="12" md="9" lg="9" xl="10">
-            <v-card
-              class="pa-4"
-              height="100%"
-              elevation="0"
-              style="
-                background: linear-gradient(
-                  rgba(35, 35, 36, 0.6),
-                  rgba(35, 35, 36, 0.8),
-                  rgba(35, 35, 36, 0.8),
-                  rgba(35, 35, 36, 0.6)
-                );
-              "
-            >
-              <v-card-title>{{ parts.title }}</v-card-title>
-              <v-card-subtitle class="mt-n1">
-                {{
-                  parts.release_date != "" ? formatDate(parts.release_date) : ""
-                }}</v-card-subtitle
-              >
-              <v-card-text>
-                <p
-                  style="
-                    display: -webkit-box;
-                    max-width: 100vw;
+      <v-list lines="two" class="ma-5">
+        <v-list-item
+          v-for="parts in collectionMovies.parts"
+          :key="parts"
+          :title="parts.title"
+          :subtitle="parts.overview"
+          border="sm"
+          rounded="xl"
+          class="mb-3"
+        >
+          <template v-slot:prepend>
+            <v-avatar size="150" rounded="lg" color="grey-lighten-1">
+              <v-img
+                :src="`https://image.tmdb.org/t/p/original${parts.poster_path}`"
+                :lazy-src="`https://image.tmdb.org/t/p/w300${parts.poster_path}`"
+                cover
+              ></v-img>
+            </v-avatar>
+          </template>
 
-                    -webkit-line-clamp: 5;
-                    -webkit-box-orient: vertical;
-                    overflow: hidden;
-                  "
-                  class="text-body-2"
-                >
-                  {{ parts.overview }}
-                </p>
-              </v-card-text>
-              <v-row no-gutters justify="end">
-                <v-btn
-                  variant="plain"
-                  @click="dialog = false"
-                  :to="{
-                    name: 'Detalhes',
-                    params: { id: parts.id, movieOrTv: parts.media_type },
-                  }"
-                  >Ver mais</v-btn
-                >
-              </v-row>
-            </v-card>
-          </v-col>
-        </v-row>
+          <v-row class="mt-12" no-gutters justify="end">
+            <v-btn
+              variant="plain"
+              @click="dialog = false"
+              :to="{
+                name: 'Detalhes',
+                params: { id: parts.id, movieOrTv: parts.media_type },
+              }"
+              >Ver mais</v-btn
+            >
+          </v-row>
+        </v-list-item>
+      </v-list>
+    </v-card>
+
+    <v-sheet v-else>
+      <v-toolbar>
+        <v-toolbar-title class="text-h5">
+          <v-row no-gutters>
+            <span :class="!useDisplay.xs ? 'text-h4' : ''">
+              {{ collectionMovies.name }}</span
+            >
+            <v-spacer></v-spacer>
+            <v-icon
+              icon="mdi-close"
+              size="small"
+              color="grey"
+              @click="dialog = false"
+            ></v-icon>
+          </v-row>
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card v-for="parts in collectionMovies.parts" :key="parts" rounded="xl" class="ma-5">
         <v-img
-          v-else
           :src="`https://image.tmdb.org/t/p/original${parts.poster_path}`"
           :lazy-src="`https://image.tmdb.org/t/p/w300${parts.poster_path}`"
           gradient="to right, rgba(0,0,0,0.6),rgba(0,0,0,0.4),rgba(0,0,0,0.3),rgba(0,0,0,0)"
           cover
-          height="100%"
         >
           <template v-slot:placeholder>
             <div class="d-flex align-center justify-center fill-height">
@@ -169,7 +144,7 @@
           </v-row>
         </v-img>
       </v-card>
-    </v-card>
+    </v-sheet>
   </v-dialog>
 </template>
 
@@ -198,7 +173,9 @@ export default {
     getWatchColection() {
       axios
         .get(
-          `${import.meta.env.VITE_BASE_URL}/collection/${this.collectionId}?api_key=${import.meta.env.VITE_API_KEY}&language=pt-BR`
+          `${import.meta.env.VITE_BASE_URL}/collection/${
+            this.collectionId
+          }?api_key=${import.meta.env.VITE_API_KEY}&language=pt-BR`
         )
         .then((response) => {
           console.log("Collections", response);
